040d0b440201b5081a4aaeed32593c87cf6a3dfe
shams261 <shamsshoaib261@gmail.com>
2026-01-14 13:49:23 -0500

    store procider wraps the app

--- frontend/clothingClientSide/src/main.jsx
+++ frontend/clothingClientSide/src/main.jsx
@@ -1,15 +1,19 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import "./index.css";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+import { StoreProvider } from "./store/storeProvider";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <StoreProvider>
+        <App />
+      </StoreProvider>
     </BrowserRouter>
-  </StrictMode>
+  </React.StrictMode>
 );
