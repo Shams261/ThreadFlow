5063e54e4f0ca3c179eee79b0bea4ed663bf00b2
Ubuntu <ubuntu@ip-172-31-29-160.ec2.internal>
2026-01-20 06:34:31 +0000

    Add CORS configuration for production

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,8 +20,11 @@ app.use(morgan("dev"));
 
 app.use(
   cors({
-    origin: process.env.CLIENT_ORIGIN || "http://localhost:5173",
-    credentials: true,
+   'http://localhost:5173',
+    'http://localhost:5174',
+    'https://itfinder-umber.vercel.app'  // Tumhara Vercel URL
+  ],
+  credentials: true
   }),
 );
 
